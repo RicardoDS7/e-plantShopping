67b6592ccada7cd5a0ebc09052738ad27ab85677
Ricardo <ricardo.desousa@gmail.com>
2024-09-16 17:36:03 -0400

    Fix the duplicated nav

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,38 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
+import ProductList from './ProductList';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
+  const [showCart, setShowCart] = useState(true); 
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    cart.forEach((item) => {
+      totalAmount += item.cost * item.quantity;
+    });
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    setShowCart(false);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({...item,quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 0) {
+        dispatch(updateQuantity({...item,quantity: item.quantity - 1}));
+    } else if (item.quantity === 0) {
+        dispatch(removeItem(item));
+    }
+    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem);
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item.quantity;
   };
 
   return (
+    <>
+    {showCart ? (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
       <div>
@@ -60,6 +79,11 @@ const CartItem = ({ onContinueShopping }) => {
         <button className="get-started-button1">Checkout</button>
       </div>
     </div>
+        ) : (
+            <ProductList onContinueShopping={handleContinueShopping}/>
+            )}
+    </>
+
   );
 };
 
